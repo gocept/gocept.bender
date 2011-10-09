7669cda14c11d454560cb202a01ee239569abbbb
Wolfgang Schnerring <ws@gocept.com>
2011-10-09 12:01:56 +0200

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.bender',
-    version='0.1dev',
+    version='1.0',
     author='Wolfgang Schnerring',
     author_email='ws@gocept.com',
     url='http://packages.python.org/gocept.bender',
